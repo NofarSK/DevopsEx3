eac580a51bdc6c811a91ab34543e89cf65cf8fcf
NofarSK <nofar1608@gmail.com>
2024-05-20 13:51:31 +0300

    update

--- NofarSkouri.java
+++ NofarSkouri.java
@@ -1,7 +1,7 @@
 public class NofarSkouri{
 	public static void main(String[] args){
 		for (int i=1 ; i<5 ; i++){
-			System.out.println("Nofar Skouri " + i);
+			System.out.println("Nofar Skouri ! " + i);
 		}
 	}
 }
